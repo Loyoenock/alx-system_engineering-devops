75031e3fb00634972e199702b6002ee1d8f57275
Loyoenock <loyoenock@gmail.com>
2023-03-24 10:47:30 -0700

    indention fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file
 file { '/temp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
