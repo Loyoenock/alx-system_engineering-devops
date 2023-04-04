53781f9a76db39fca10a7e81c27fba0427627459
Loyoenock <loyoenock@gmail.com>
2023-04-04 04:13:50 +0300

    2-puppet_custom_http_response_header.pp syntax binding

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
 # automating creating a custom HTTP header response with Puppet.
 
 exec { 'command':
-  command => 'apt-get -y install -y update;
+  command => 'apt-get -y install -y && apt-get update;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
