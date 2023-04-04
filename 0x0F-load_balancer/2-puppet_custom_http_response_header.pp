26864b0451840450859ac679cc53bd21392afd0a
Loyoenock <loyoenock@gmail.com>
2023-04-04 04:25:52 +0300

    2-puppet_custom_http_response_header.pp syntax error fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 # automating creating a custom HTTP header response with Puppet.
 
 exec { 'command':
-  command => 'apt-get -y install -y && apt-get update;
+  command => 'sudo apt-get -y install -y && apt-get update;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
+  sudo service nginx restart',
   provider => shell,
 }
