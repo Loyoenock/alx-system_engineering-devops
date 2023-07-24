9bb3ad561862f9a703b7c32e5863e35229b3d7d0
Loyoenock <loyoenock@gmail.com>
2023-07-24 11:39:21 -0700

    pycodestyle validation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,6 @@ import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
@@ -19,3 +18,4 @@ if __name__ == "__main__":
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow([user_id, username, t.get("completed"), t.get("title")])
          for t in todos]
+
