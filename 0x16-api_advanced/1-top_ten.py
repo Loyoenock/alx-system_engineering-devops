a274488feea24430331ff094d57e31745c314a0b
Loyoenock <loyoenock@gmail.com>
2023-08-08 14:15:06 +0000

    Pep 8 style fix - white space

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 """
-A function that queries the Reddit API and prints the 
+A function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
@@ -22,3 +21,4 @@ def top_ten(subreddit):
         return
     results = response.json().get("data")
     [print(child.get("data").get("title")) for child in results.get("children")]
+
